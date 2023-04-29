b492a4cf70d20fca9f5b4e4312faa60dfb2c12f9
Louisetrovato <73744715+Louisetrovato@users.noreply.github.com>
2023-04-29 21:28:17 +0200

    poprawki

--- src/App.js
+++ src/App.js
@@ -6,24 +6,23 @@ function App() {
     <div className="App">
       <div id="not-found-div">
         <span id="not-found-span">404 NOT FOUND</span>
-        <div id="container">
-          <img src={photo} id="photo" alt="witcher"></img>
-          <div id="box">
-            <h1>I have bad news for you</h1>
-            <div id="p-box">
-              <p>
-                The page you are looking for might be removed or is temporarily
-                unavailable
-              </p>
-            </div>
-
+      </div>
+      <div id="container">
+        <img src={photo} id="photo" alt="witcher"></img>
+        <div id="box">
+          <h1>I have bad news for you</h1>
+          <div id="p-box">
+            <p>
+              The page you are looking for might be removed or is temporarily
+              unavailable
+            </p>
+          </div>
+          <div id="button">
             <button id="btn">BACK TO HOMEPAGE</button>
           </div>
         </div>
-        <footer id="footer">
-          Created by <span id="bold-footer">EdiCymer</span> - devChallanges.io
-        </footer>
       </div>
+      <div id="footer">Created by EdiCymer - devChallanges.io</div>
     </div>
   );
 }
